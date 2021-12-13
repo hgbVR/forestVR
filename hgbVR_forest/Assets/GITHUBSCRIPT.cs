74796795e91ec9a8c6331461c8574b2bfa441137
hgbVR <s2110745008@students.fh-hagenberg.at>
2021-12-13 17:25:12 +0100

    test1

---
+++ hgbVR_forest/Assets/GITHUBSCRIPT.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GITHUBSCRIPT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //git hub test
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
