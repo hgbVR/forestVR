dfd64fb62f44e3545af2127dddcc89da1436e153
vickyfrwickey <viktoria_frank@yahoo.com>
2021-12-22 19:25:47 +0100

    neues Update

---
+++ hgbVR_forest/Assets/MyTriggerSoundOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTriggerSoundOff : MonoBehaviour
+{
+
+    public AudioSource playWorldSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playWorldSound.Stop();
+    }
+}
