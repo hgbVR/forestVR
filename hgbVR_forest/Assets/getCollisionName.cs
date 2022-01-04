4e5a414fb5a7ad4cf210f769e68f27296f18b5ca
hgbVR <s2110745008@students.fh-hagenberg.at>
2022-01-04 19:48:29 +0100

    collision_get_name

---
+++ hgbVR_forest/Assets/getCollisionName.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getCollisionName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+    void OnCollisionEnter(Collision thecollision)
+    {
+        Debug.Log("Collision");
+        Debug.Log(thecollision.gameObject.name);
+
+    }
+    */
+
+    void OnTriggerEnter(Collider thecollider)
+    {
+        Debug.Log("triggerEnter");
+        Debug.Log(thecollider.gameObject.name);
+
+
+        //hier den counter triggern
+
+        /*
+        if (thecollider.gameObject.tag == "triggerCube")
+        {
+            Debug.Log("tagging works");            
+           
+
+        }
+        */
+    }
+
+}
--- hgbVR_forest/Assets/timerButton.cs
+++ hgbVR_forest/Assets/timerButton.cs
@@ -13,8 +13,10 @@ public class timerButton : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         currentTime = 0;
+        StartTimer();
     }
 
+    
     // Update is called once per frame
     void Update(){
         if(timerActive == true){
@@ -23,6 +25,19 @@ public class timerButton : MonoBehaviour
         TimeSpan time = TimeSpan.FromSeconds(currentTime);
         currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
     }
+    
+    /*
+    // Update is called once per frame
+    void Update()
+    {        
+        currentTime = currentTime + Time.deltaTime;
+        Debug.Log(currentTime);
+
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+    }
+    */
+
 
     public void StartTimer(){
         timerActive = true;
